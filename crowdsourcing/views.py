f59e84465835b505ea8c76e29c7d97cab6718d10
Elsa Bakiu <Elsa.Bakiu@pentlandfirth.com>
2015-04-08 18:38:59 +0200

    User Profile front end

--- crowdsourcing/views.py
+++ crowdsourcing/views.py
@@ -340,4 +340,4 @@ class UserProfile(TemplateView):
         self.user_profile = get_model_or_none(models.UserProfile, username=kwargs['username'])
         if self.user_profile is None:
             return render(request,'404.html')
-        return render(request, 'profile.html', {'user_profile': self.user_profile})
+        return render(request, 'profile.html', {'user': self.user_profile})
